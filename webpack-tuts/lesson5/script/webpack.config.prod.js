97c6b94989eb2a85cb7c7ca7bf26bfa9b60b0d2a
Link <ylin278@wisc.edu>
2020-04-23 15:08:44 -0500

    lesson5 add babel

---
+++ webpack-tuts/lesson5/script/webpack.config.prod.js
@@ -0,0 +1,64 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: "./src/index.js",
+    },
+    output: {
+        path: path.resolve(process.cwd(), "dist"),
+        filename: "js/[name].[chunkHash:6].js",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "webpack",
+            template: "public/index.html",
+        }),
+        new MiniCssExtractPlugin({
+            filename: "static/css/[name].[chunkHash:6].css",
+        }),
+        new CopyPlugin([{
+            from: path.resolve(process.cwd(), 'src/static'),
+            to: path.resolve(process.cwd(), 'dist/static')
+        }])
+    ],
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8000,
+                        name: 'static/images/[name].[ext]',
+                        publicPath: '/',
+                    },
+                }, ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+
+        ],
+    },
+    devServer: {
+        port: 8080,
+        open: true,
+    },
+};
--- webpack-tuts/lesson5/src/index.js
+++ webpack-tuts/lesson5/src/index.js
@@ -1,3 +1,6 @@
 import './index.scss'
 
+import { add } from './test'
+add(1, 2)
+
 console.log('abcdefg welcome');
