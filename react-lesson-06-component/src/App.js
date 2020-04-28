17253a0f89f16eaf8cbeee42d164db5205fb49f9
Link <ylin278@wisc.edu>
2020-04-28 02:09:51 -0500

    AddTodo state 子夫组件传至

--- react-lesson-06-component/src/App.js
+++ react-lesson-06-component/src/App.js
@@ -12,29 +12,47 @@ class App extends Component {
         {
           id: 1,
           title: "吃饭",
-          assignee: 'link',
+          assignee: "link",
           isCompleted: true,
         },
         {
           id: 2,
           title: "睡觉",
-          assignee: 'link',
+          assignee: "link",
           isCompleted: false,
         },
       ],
     };
   }
 
+  addTodo = (todoTitle) => {
+    //   console.log(todoTitle)
+    //   this.setState({
+    //       todos: this.state.todos.concat({
+    //           id: Math.random(),
+    //           title: todoTitle,
+    //           isCompleted: false
+    //       })
+    //   })
+    const newTodos = this.state.todos.slice();
+    newTodos.push({
+      id: newTodos.length+1,
+      title: todoTitle,
+      isCompleted: false,
+    });
+    this.setState({
+        todos : newTodos
+    })
+  };
+
   render() {
     return (
       <div className="abc">
-       {
-        <div dangerouslySetInnerHTML={{__html:this.state.article}} />
-       }
+        {<div dangerouslySetInnerHTML={{ __html: this.state.article }} />}
         <TodoHeader desc={this.state.title} x={1} y={2}>
           {this.state.desc}
         </TodoHeader>
-        <TodoInput btnText="Add" />
+        <TodoInput btnText="Add" addTodo={this.addTodo} />
         <TodoList todos={this.state.todos} />
         <Like />
       </div>
--- react-lesson-06-component/src/components/TodoInput/TodoInput.js
+++ react-lesson-06-component/src/components/TodoInput/TodoInput.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
 
 export default class TodoInput extends Component {
+    constructor(){
+        super()
+        this.state = {
+            inputValue: ''
+        }
+    }
     render() {
         return (
             <div>
-                <input type="text" /><button> {this.props.btnText} </button>
+                <input type="text" value={this.state.inputValue}
+                onChange={this.handleInput}/>
+                <button onClick={this.handleAddClick}> {this.props.btnText} </button>
             </div>
         )
+    }
+
+    handleInput = (e)=>{
+        console.log(e.currentTarget.value)
+        this.setState({
+            inputValue: e.currentTarget.value
+        })
+    }
+
+    handleAddClick = () => {
+        this.props.addTodo(this.state.inputValue)
     }
 }
