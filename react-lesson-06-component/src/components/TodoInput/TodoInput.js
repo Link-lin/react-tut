8926afbad517d1f868ed8c86d691cfed1453d47d
Link <ylin278@wisc.edu>
2020-04-29 15:18:01 -0500

    return add refocus

--- react-lesson-06-component/src/components/TodoInput/TodoInput.js
+++ react-lesson-06-component/src/components/TodoInput/TodoInput.js
@@ -1,30 +1,54 @@
-import React, { Component } from 'react'
+// React 通过 ref 来获取组件或者dom 元素，
+import React, { Component, createRef } from "react";
 
 export default class TodoInput extends Component {
-    constructor(){
-        super()
-        this.state = {
-            inputValue: ''
-        }
-    }
-    render() {
-        return (
-            <div>
-                <input type="text" value={this.state.inputValue}
-                onChange={this.handleInput}/>
-                <button onClick={this.handleAddClick}> {this.props.btnText} </button>
-            </div>
-        )
-    }
+  constructor() {
+    super();
+    this.state = {
+      inputValue: "",
+    };
+    this.inputBox = createRef();
+  }
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.inputValue}
+          onChange={this.handleInput}
+          onKeyUp={this.handleKeyUp}
+          ref={this.inputBox}
+        />
+        <button onClick={this.handleAddClick}> {this.props.btnText} </button>
+      </div>
+    );
+  }
 
-    handleInput = (e)=>{
-        console.log(e.currentTarget.value)
-        this.setState({
-            inputValue: e.currentTarget.value
-        })
-    }
+  handleInput = (e) => {
+    console.log(e.currentTarget.value);
+    this.setState({
+      inputValue: e.currentTarget.value,
+    });
+  };
 
-    handleAddClick = () => {
-        this.props.addTodo(this.state.inputValue)
+  handleKeyUp = (e) => {
+    if (e.keyCode === 13) {
+      this.handleAddClick();
     }
+  };
+
+  handleAddClick = (e) => {
+    if (this.state.inputValue === "") {
+      return;
+    }
+    this.props.addTodo(this.state.inputValue);
+    this.setState(
+      {
+        inputValue: "",
+      },
+      () => {
+        this.inputBox.current.focus();
+      }
+    );
+  };
 }
