4f5456708083aaf96a14c9ed170cbc1a60086e63
Link <ylin278@wisc.edu>
2020-05-07 16:50:54 -0500

    lifetime completed

---
+++ react-lesson-08-ajax/src/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import { TodoHeader, TodoInput, TodoList, Like } from "./components/index";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "待办事项",
+      desc: "今日事今日毕",
+      article: "<div>hello</div>",
+      todos: [
+        {
+          id: 1,
+          title: "吃饭",
+          assignee: "link",
+          isCompleted: true,
+        },
+        {
+          id: 2,
+          title: "睡觉",
+          assignee: "link",
+          isCompleted: false,
+        },
+      ],
+    };
+  }
+
+  onCompletedChange = (id) => {
+    console.log(id);
+    this.setState((prevState) => {
+      return {
+        todos: this.state.todos.map((todo) => {
+          if (todo.id === id) {
+            todo.isCompleted = !todo.isCompleted;
+          }
+          return todo
+        }),
+      };
+    });
+  };
+
+  addTodo = (todoTitle) => {
+    //   console.log(todoTitle)
+    //   this.setState({
+    //       todos: this.state.todos.concat({
+    //           id: Math.random(),
+    //           title: todoTitle,
+    //           isCompleted: false
+    //       })
+    //   })
+    const newTodos = this.state.todos.slice();
+    newTodos.push({
+      id: newTodos.length + 1,
+      title: todoTitle,
+      isCompleted: false,
+    });
+    this.setState({
+      todos: newTodos,
+    });
+  };
+
+  render() {
+    return (
+      <div className="abc">
+        {<div dangerouslySetInnerHTML={{ __html: this.state.article }} />}
+        <TodoHeader desc={this.state.title} x={1} y={2}>
+          {this.state.desc}
+        </TodoHeader>
+        <TodoInput btnText="Add" addTodo={this.addTodo} />
+        <TodoList
+          todos={this.state.todos}
+          onCompletedChange={this.onCompletedChange}
+        />
+        <Like />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react-lesson-08-ajax/src/components/TodoList/TodoList.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import TodoItem from "./TodoItem";
+import PropTypes from "prop-types";
+
+export default class TodoList extends Component {
+  static propTypes = {
+      todos: PropTypes.arrayOf(PropTypes.shape({
+          id: PropTypes.number.isRequired,
+          title:PropTypes.string.isRequired,
+          isCompleted: PropTypes.bool.isRequired
+      })).isRequired
+  }
+  render() {
+    return (
+      <ul>
+        {this.props.todos.map((todo) => {
+          return <TodoItem key={todo.id} {...todo} onCompletedChange={this.props.onCompletedChange} />;
+        })}
+      </ul>
+    );
+  }
+}
