5cbbe3ba7db9eacf339d6472af38575021f25c7c
Link <ylin278@wisc.edu>
2020-05-07 23:22:49 -0500

    hooks done

--- react-lesson-09-hooks/src/index.js
+++ react-lesson-09-hooks/src/index.js
@@ -1,17 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 
-const Counter = () =>{
-    return (
-        <div>
-            <button>-</button> 
-            <span>10</span>
-            <button>+</button> 
-        </div>
-    )
-}
+const Counter = () => {
+  console.log(useState(10));
+  const [count, setCount] = useState(0);
 
-ReactDOM.render(
-    <Counter/>, document.getElementById('root')
-)
+  useEffect(() =>{
+    console.log('更新了');
+    // eslint-disable-next-line no-template-curly-in-string
+    document.title = `当前的数量为${count}`;
+  })
+  return (
+    <div>
+      <p>当前的数量为{count}</p>
+      <button onClick={() => setCount(count - 1)}>-</button>
+      <span>{count}</span>
+      <button onClick={() => setCount(count + 1)}>+</button>
+    </div>
+  );
+};
 
+ReactDOM.render(<Counter />, document.getElementById("root"));
