5d6b81f97dfc8292b608656463bde58c4281432c
Link <ylin278@wisc.edu>
2020-05-20 16:34:28 -0500

    manually passed redux

--- react-lesson-13-redux/src/index.js
+++ react-lesson-13-redux/src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import store from "./store";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+window.store = store;
+console.log(store);
+ReactDOM.render(<App store={store}/>, document.getElementById("root"));
